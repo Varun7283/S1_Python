bd874e9a6701da730507713ed1bfd28f87362451
00midhun00 <midhuntjoseph03@gmail.com>
2025-09-18 15:29:09 +0530

    first commit

---
+++ varun.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+int age;
+char name(),place();
+printf("Enter your name");
+scanf("%s,&name");
+printf("Enter Your age");
+scanf("%d,age");
+printf("Enter Your place");
+scanf("%s,&name");
+printf("%s,%d,%s",name,age,place);
+return(0);
+}
